bd9aaa7e657b4b9102e849bb97d5e9c8b31832fc
Kaycheng <kaycheng717@gmail.com>
2019-08-12 16:45:15 +0800

    Add username on user table and set sign in/sign out button

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_12_072333) do
+ActiveRecord::Schema.define(version: 2019_08_12_083313) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,8 +50,10 @@ ActiveRecord::Schema.define(version: 2019_08_12_072333) do
     t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
